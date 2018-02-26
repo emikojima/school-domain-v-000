388a5c31ef22b73daf924a4978a1e0d182d202ea
emi kojima <emi8emi@gmail.com>
2018-02-26 22:16:12 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster, :grade
+  
+  def initialize(new)
+    @roster={}
+  end
+  
+  def add_student(student_name, grade)
+    if !@roster.keys.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+   @roster[grade]
+  end
+  
+  def sort
+      @roster = @roster.sort
+      roster.each do |grade, name|
+      
+      end
+      roster
+      
+     
+  end
+   
+     
+   
+  
+end
+  
+
+    
